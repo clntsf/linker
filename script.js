22d150edd0b02e028d761e01ae3ddaa579b800cb
ctsf1 <79168898+ctsf1@users.noreply.github.com>
2021-08-09 15:17:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function getRedirectLink(){
     args = atob(window.location.search.substring(1)).split('%')
     $('head').append(`<title>${args[1]}</title>`)
     if (args != ''){
-//         try {window.location.href = args[0]}
-//         catch(e){}
+        try {window.location.href = args[0]}
+        catch(e){}
     }
     return
 }
