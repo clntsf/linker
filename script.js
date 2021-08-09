b613f433e296938c38db3eaf5a5c3b6b14bc25b5
ctsf1 <79168898+ctsf1@users.noreply.github.com>
2021-08-09 01:50:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function getRedirectLink(){
-    args = window.location.search.substring(1);
+    args = window.location.search.substring(1)
     if (args != ''){
-        try {window.location.href = atob(args)}
+        try {
+            dec = atob(args).split('%');
+            document.title = dec[1]
+            window.location.href = dec[0]
+        }
         catch(e){}
     }
     return
 }
 
 function encodeStr(){
-    str_input = document.getElementById("input").value;
-    document.getElementById("output").innerHTML = `ctsf1.github.io/linker/index.html?${btoa(str_input)}`;
+    link= document.getElementById("link-input").value; title = document.getElementById("title-input").value;
+    document.getElementById("output").innerHTML = `ctsf1.github.io/linker/index.html?${btoa(link+'%'+title)}`;
 }
-
