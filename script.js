e2e3f5d5b08b712d024f5b3ecb9bff306a861d10
ctsf1 <79168898+ctsf1@users.noreply.github.com>
2021-08-09 00:50:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function getRedirectLink(){
+    args = window.location.search.substring(1);
+    if (args != ''){
+        try {window.location.href = atob(args)}
+        catch(e){}
+    }
+    return
+}
+
+function encodeStr(){
+    str_input = document.getElementById("input").value;
+    document.getElementById("output").innerHTML = `ctsf1.github.io/linker/index.html?${btoa(str_input)}`;
+}
